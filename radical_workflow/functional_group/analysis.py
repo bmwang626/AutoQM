801f146570a659e94cdf85976ec0c8455548209f
Hao-Wei Pang <hwpang@mit.edu>
2023-03-30 16:04:32 -0400

    update functional group code

--- radical_workflow/functional_group/analysis.py
+++ radical_workflow/functional_group/analysis.py
@@ -4,46 +4,48 @@ from rmgpy.molecule.group import GroupAtom, Group, GroupBond
 def functional_group_analysis(smiles, max_num_heavy_atoms_in_functional_group=5):
     functional_group_smiles_set = set()
 
-    mol = make_rmg_mol(smiles)
+    molecule = make_rmg_mol(smiles)
 
-    if mol is None:
+    if molecule is None:
         return functional_group_smiles_set
     
-    sssr = mol.get_smallest_set_of_smallest_rings()
-    monorings, polyrings = mol.get_disparate_cycles()
+    sssr = molecule.get_smallest_set_of_smallest_rings()
+    monorings, polyrings = molecule.get_disparate_cycles()
     all_rings = sssr + monorings + polyrings
     
     all_ring_atoms = set()
     for ring in all_rings:
         all_ring_atoms.update(ring)
+
+    sampled_functional_group_smiles_set = get_ring_functional_groups(molecule, rings=all_rings)
+    functional_group_smiles_set.update(sampled_functional_group_smiles_set)
     
-    for atom in mol.atoms:
-        if not atom.is_hydrogen() and not atom.is_halogen() and atom not in all_ring_atoms:
-            sampled_functional_group_smiles = get_n_radius_functional_group(atom, mol, max_num_heavy_atoms_in_functional_group=max_num_heavy_atoms_in_functional_group)
+    for atom in molecule.atoms:
+        if not atom.is_hydrogen() and not atom.is_halogen():
+            sampled_functional_group_smiles = get_n_radius_functional_group(atom, molecule, all_ring_atoms, max_num_heavy_atoms_in_functional_group=max_num_heavy_atoms_in_functional_group)
             if sampled_functional_group_smiles is not None:
                 functional_group_smiles_set.add(sampled_functional_group_smiles)
-
-    sampled_functional_group_smiles_set = get_ring_functional_groups(mol, rings=all_rings)
-    functional_group_smiles_set.update(sampled_functional_group_smiles_set)
 
     return functional_group_smiles_set
 
 def make_rmg_mol(smiles):
     try:
-        mol = Molecule().from_smiles(smiles)
+        molecule = Molecule().from_smiles(smiles)
     except:
         print(f"Could not parse smiles: {smiles}")
         return None
-    mol.sort_atoms()
-    return mol
+    molecule.sort_atoms()
+    return molecule
 
-def get_n_radius_functional_group(center_atom, mol, max_num_heavy_atoms_in_functional_group):
-
+def get_n_radius_functional_group(center_atom, molecule, all_ring_atoms, max_num_heavy_atoms_in_functional_group):
+    if center_atom in all_ring_atoms:
+        return None
+    
     sampled_functional_group_smiles = None
-    num_heavy_atoms = sum(not atom.is_hydrogen() for atom in mol.atoms)
+    num_heavy_atoms = sum(not atom.is_hydrogen() for atom in molecule.atoms)
     max_n_radius_neighbor = min(max_num_heavy_atoms_in_functional_group, num_heavy_atoms)
     for n_radius_neighbor in range(1, max_n_radius_neighbor + 1):
-        group = make_group(center_atom, mol, n_radius_neighbor)
+        group = make_group(center_atom, molecule, all_ring_atoms, n_radius_neighbor)
         sampled_mol = group.make_sample_molecule()
         sampled_mol.sort_atoms()
         if sum(not atom.is_hydrogen() for atom in sampled_mol.atoms) <=max_num_heavy_atoms_in_functional_group:
@@ -52,9 +54,7 @@ def get_n_radius_functional_group(center_atom, mol, max_num_heavy_atoms_in_funct
             break
     return sampled_functional_group_smiles
 
-def make_group(center_atom, molecule, n_radius_neighbor=1):
-    
-    aromatic_rings, aromatic_bonds = molecule.get_aromatic_rings()
+def make_group(center_atom, molecule, all_ring_atoms, n_radius_neighbor=1):
 
     group_atoms = {}
     group_atoms[center_atom] = GroupAtom(atomtype=[center_atom.element.symbol])
@@ -62,7 +62,7 @@ def make_group(center_atom, molecule, n_radius_neighbor=1):
     neighbors = list(center_atom.edges.items())
     neighbors.sort()
 
-    group_atoms = get_neighbors(neighbors, aromatic_rings, group_atoms, n_radius_neighbor, 1)
+    group_atoms = get_neighbors(neighbors, group_atoms, all_ring_atoms, n_radius_neighbor, 1)
     
     group = Group(atoms=list(group_atoms.values()))
 
@@ -75,21 +75,16 @@ def make_group(center_atom, molecule, n_radius_neighbor=1):
     return group
 
 
-def get_neighbors(atoms, aromatic_rings, group_atoms, n_radius_neighbor, degree):
+def get_neighbors(atoms, group_atoms, all_ring_atoms, n_radius_neighbor, degree):
     
     for (atom, bond) in atoms:
-        if atom not in group_atoms:
+        if atom not in group_atoms and atom not in all_ring_atoms:
             group_atoms[atom] = GroupAtom(atomtype=[atom.element.symbol])
-            for ring in aromatic_rings: #include full aromatic rings to avoid using generate resonance structures (which is slow for some cases)
-                if atom in ring:
-                    for ring_atom in ring:
-                        if ring_atom not in group_atoms:
-                            group_atoms[ring_atom] = GroupAtom(atomtype=[ring_atom.element.symbol])
 
         if degree + 1 <= n_radius_neighbor:
             neighbors = list(item for item in atom.edges.items() if not item[0].is_hydrogen())
             neighbors.sort()
-            group_atoms = get_neighbors(neighbors, aromatic_rings, group_atoms, n_radius_neighbor, degree + 1)
+            group_atoms = get_neighbors(neighbors, group_atoms, all_ring_atoms, n_radius_neighbor, degree + 1)
             
     return group_atoms
 
