1c631c3f860b018da9f98dbdd16891a1ee8acc22
Hao-Wei Pang <hwpang@mit.edu>
2023-03-30 14:42:52 -0400

    update functional group stuff

--- scripts/functional_group/perform_functional_group_analysis.py
+++ scripts/functional_group/perform_functional_group_analysis.py
@@ -15,16 +15,21 @@ parser.add_argument('--n_jobs', type=int, default=1,
 args = parser.parse_args()
 
 # load smiles
+print('Loading smiles...')
 df_smiles = pd.read_csv(args.input_smiles, index_col=0)
 smiles_list = df_smiles['smiles'].tolist()
 
 # perform functional group analysis
+print('Performing functional group analysis...')
 outs = Parallel(n_jobs=args.n_jobs, backend="multiprocessing")(delayed(functional_group_analysis)(smi) for smi in tqdm(smiles_list))
+
+# collect results
 functional_group_smiles = set()
 for out in outs:
     functional_group_smiles.update(out)
 
 # save results
+print('Saving results...')
 df_groups = pd.DataFrame(functional_group_smiles, columns=['functional_group_smiles'])
 input_file_name = os.path.basename(args.input_smiles)
 output_file_name = input_file_name.replace('.csv', '_functional_groups.csv')
