0e6ca8c07e7e77446fa7a6b1ebceafbb316c1fed
Hao-Wei Pang <hwpang@mit.edu>
2023-03-30 16:43:03 -0400

    update functional group code

--- radical_workflow/functional_group/analysis.py
+++ radical_workflow/functional_group/analysis.py
@@ -82,8 +82,8 @@ def get_neighbors(atoms, group_atoms, all_ring_atoms, n_radius_neighbor, degree)
         if atom not in group_atoms:
             group_atoms[atom] = GroupAtom(atomtype=[atom.atomtype])
 
-            if atom in all_ring_atoms:
-                continue
+        if atom in all_ring_atoms:
+            continue
 
         if degree + 1 <= n_radius_neighbor:
             neighbors = list(item for item in atom.edges.items() if not item[0].is_hydrogen())
