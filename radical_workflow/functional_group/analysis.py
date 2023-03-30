52a555e7e9841bbd95e1261ecc0bbce9e0e872db
Hao-Wei Pang <hwpang@mit.edu>
2023-03-30 14:27:54 -0400

    add functional group analysis code

---
+++ radical_workflow/functional_group/analysis.py
@@ -0,0 +1,98 @@
+from rmgpy.molecule.molecule import Molecule
+from rmgpy.molecule.group import GroupAtom, Group, GroupBond
+
+def functional_group_analysis(smiles, max_num_heavy_atoms_in_functional_group=5):
+    sampled_functional_group_smiles = set()
+
+    mol = make_rmg_mol(smiles)
+    
+    for atom in mol.atoms:
+        if not atom.is_hydrogen() and not atom.is_halogen():
+            sampled_functional_group_smiles = get_functional_group(atom, mol, max_num_heavy_atoms_in_functional_group=max_num_heavy_atoms_in_functional_group)
+            if sampled_functional_group_smiles is not None:
+                sampled_functional_group_smiles.add(sampled_functional_group_smiles)
+
+    return sampled_functional_group_smiles
+
+def make_rmg_mol(smiles):
+    mol = Molecule().from_smiles(smiles)
+    mol.sort_atoms()
+    return mol
+
+def get_functional_group(center_atom, mol, max_num_heavy_atoms_in_functional_group):
+    sampled_functional_group_smiles = None
+    num_heavy_atoms = sum(not atom.is_hydrogen() for atom in mol.atoms)
+    max_n_radius_neighbor = min(max_num_heavy_atoms_in_functional_group, num_heavy_atoms)
+    for n_radius_neighbor in range(1, max_n_radius_neighbor + 1):
+        group = make_group(center_atom, mol, n_radius_neighbor)
+        sampled_mol = group.make_sample_molecule()
+        sampled_mol.sort_atoms()
+        if sum(not atom.is_hydrogen() for atom in sampled_mol.atoms) <=max_num_heavy_atoms_in_functional_group:
+            sampled_functional_group_smiles = sampled_mol.to_smiles()
+        else:
+            break
+    return sampled_functional_group_smiles
+
+def make_group(center_atom, molecule, n_radius_neighbor=1):
+    
+    aromatic_rings, aromatic_bonds = molecule.get_aromatic_rings()
+
+    group_atoms = {}
+    group_atoms[center_atom] = GroupAtom(atomtype=[center_atom.atomtype],
+                                         radical_electrons=[center_atom.radical_electrons],
+                                         lone_pairs=[center_atom.lone_pairs],
+                                         charge=[center_atom.charge],
+                                         label='*')
+    
+    neighbors = list(center_atom.edges.items())
+    neighbors.sort()
+
+    group_atoms = get_neighbors(neighbors, aromatic_rings, group_atoms, n_radius_neighbor, 1)
+    
+    group = Group(atoms=list(group_atoms.values()))
+
+    group = make_bonds(molecule, group, group_atoms)
+    
+    group.atoms = group.sort_by_connectivity(group.atoms)
+    
+    group.update()
+
+    return group
+
+
+def get_neighbors(atoms, aromatic_rings, group_atoms, n_radius_neighbor, degree):
+    
+    for (atom, bond) in atoms:
+        if atom not in group_atoms:
+            group_atoms[atom] = GroupAtom(atomtype=[atom.atomtype],
+                                         radical_electrons=[atom.radical_electrons],
+                                         lone_pairs=[atom.lone_pairs],
+                                         charge=[atom.charge],
+                                         label='')
+            for ring in aromatic_rings: #include full aromatic rings to avoid using generate resonance structures (which is slow for some cases)
+                if atom in ring:
+                    for ring_atom in ring:
+                        if ring_atom not in group_atoms:
+                            group_atoms[ring_atom] = GroupAtom(atomtype=[ring_atom.atomtype],
+                                                             radical_electrons=[atom.radical_electrons],
+                                                             lone_pairs=[atom.lone_pairs],
+                                                             charge=[atom.charge],
+                                                             label='')
+
+        if degree + 1 <= n_radius_neighbor:
+            neighbors = list(item for item in atom.edges.items() if not item[0].is_hydrogen())
+            neighbors.sort()
+            group_atoms = get_neighbors(neighbors, aromatic_rings, group_atoms, n_radius_neighbor, degree + 1)
+            
+    return group_atoms
+
+def make_bonds(molecule, group, group_atoms):
+    
+    for atom1, group_atom1 in group_atoms.items():
+        for atom2, group_atom2 in group_atoms.items():
+            if molecule.has_bond(atom1, atom2):
+                bond = molecule.get_bond(atom1, atom2)
+                if not group.has_bond(group_atom1,group_atom2):
+                    group.add_bond(GroupBond(group_atom1,group_atom2,order=[bond.order]))
+            
+    return group
---
+++ scripts/functional_group/perform_functional_group_analysis.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+from argparse import ArgumentParser
+from joblib import Parallel, delayed
+
+from radical_workflow.functional_group.analysis import functional_group_analysis
+
+parser = ArgumentParser()
+parser.add_argument('--input_smiles', type=str, required=True,
+                    help='input smiles included in a .csv file')
+parser.add_argument('--n_jobs', type=int, default=1,
+                    help='number of workers to use')
+
+args = parser.parse_args()
+
+# load smiles
+df_smiles = pd.read_csv(args.input_smiles, index_col=0)
+smiles_list = df_smiles['smiles'].tolist()
+
+# perform functional group analysis
+outs = Parallel(n_jobs=args.n_jobs, backend="multiprocessing")(delayed(functional_group_analysis)(smi) for smi in smiles_list)
+functional_group_smiles = set()
+for out in outs:
+    functional_group_smiles.update(out)
+
+# save results
+df_groups = pd.DataFrame(functional_group_smiles, columns=['functional_group_smiles'])
+input_file_name = os.path.basename(args.input_smiles)
+output_file_name = input_file_name.replace('.csv', '_functional_groups.csv')
+df_groups.to_csv(output_file_name)
