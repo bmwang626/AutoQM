51de238b3c3d38625a4825797b76409f83c19455
Hao-Wei Pang <hwpang@mit.edu>
2023-03-31 13:42:20 -0400

    remove maxcore error when restart

--- scripts/calculation/main_make_dlpno_sp_input.py
+++ scripts/calculation/main_make_dlpno_sp_input.py
@@ -121,6 +121,7 @@ for mol_id, smi in mol_ids_smis[args.task_id::args.num_tasks]:
             # check if maxcore error
             if has_max_core_error(log_path):
                 print(f"maxcore error for {mol_id}, removing...")
+                os.remove(log_path)
         if not os.path.exists(log_path):
             os.makedirs(subinputs_dir, exist_ok=True)
             mol_id_path = os.path.join(subinputs_dir, f"{mol_id}.in")
