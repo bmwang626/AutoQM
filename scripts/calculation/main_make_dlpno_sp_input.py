36c4cb306fe71dfce63696739a72b0c3682fa03a
Hao-Wei Pang <hwpang@mit.edu>
2023-05-22 10:50:05 -0400

    turn on max core error checking

--- scripts/calculation/main_make_dlpno_sp_input.py
+++ scripts/calculation/main_make_dlpno_sp_input.py
@@ -146,22 +146,22 @@ for mol_id, smi in mol_ids_smis[args.task_id::args.num_tasks]:
     DLPNO_level_of_theory = args.DLPNO_level_of_theory
     if mol_id in xyz_DFT_opt_dict:
         if os.path.exists(log_path):
-            # # check if maxcore error
-            # if has_max_core_error(log_path):
-            #     print(f"maxcore error for {mol_id}, removing...")
-            #     try:
-            #         os.remove(log_path)
-            #     except FileNotFoundError:
-            #         print(f"file {log_path} not found, already removed?")
-            # check if wave function error
-            if has_wave_function_error(log_path):
-                print(f"wave function error for {mol_id}, removing...")
+            # check if maxcore error
+            if has_max_core_error(log_path):
+                print(f"maxcore error for {mol_id}, removing...")
                 try:
                     os.remove(log_path)
-                    if args.DLPNO_level_of_theory == "uHF UNO DLPNO-CCSD(T)-F12D cc-pvtz-f12 def2/J cc-pvqz/c cc-pvqz-f12-cabs RIJCOSX VeryTightSCF NormalPNO":
-                        DLPNO_level_of_theory = "uHF UNO DLPNO-CCSD(T)-F12D cc-pvtz-f12 def2/J cc-pvqz/c cc-pvqz-f12-cabs RIJCOSX NormalSCF NormalPNO"
                 except FileNotFoundError:
                     print(f"file {log_path} not found, already removed?")
+            # check if wave function error
+            # if has_wave_function_error(log_path):
+            #     print(f"wave function error for {mol_id}, removing...")
+            #     try:
+            #         os.remove(log_path)
+            #         if args.DLPNO_level_of_theory == "uHF UNO DLPNO-CCSD(T)-F12D cc-pvtz-f12 def2/J cc-pvqz/c cc-pvqz-f12-cabs RIJCOSX VeryTightSCF NormalPNO":
+            #             DLPNO_level_of_theory = "uHF UNO DLPNO-CCSD(T)-F12D cc-pvtz-f12 def2/J cc-pvqz/c cc-pvqz-f12-cabs RIJCOSX NormalSCF NormalPNO"
+            #     except FileNotFoundError:
+            #         print(f"file {log_path} not found, already removed?")
         if not os.path.exists(log_path):
             os.makedirs(subinputs_dir, exist_ok=True)
             mol_id_path = os.path.join(subinputs_dir, f"{mol_id}.in")
