dc5c9808c76ca28f901210002150f4ede3b5a08b
Hao-Wei Pang <hwpang@mit.edu>
2023-10-16 13:55:25 -0400

    Update cosmo result parser

--- scripts/parsing/parse_cosmo_results.py
+++ scripts/parsing/parse_cosmo_results.py
@@ -7,25 +7,28 @@ from tqdm import tqdm
 from radical_workflow.parser.cosmo_parser import cosmo_parser
 
 
-def main(input_smiles_path, output_file_name, n_jobs, solvent_path):
+def main(input_smiles_path, output_file_name, n_jobs, solvent_path, output_dir):
     submit_dir = os.getcwd()
 
     df_solute = pd.read_csv(input_smiles_path)
     mol_ids = list(df_solute.id)
     if "smiles" in df_solute.columns:
         mol_smis = list(df_solute.smiles)
-    else:
+    elif "smi" in df_solute.columns:
+        mol_smis = list(df_solute.smi)
+    elif "rxn_smi" in df_solute.columns:
         mol_smis = list(df_solute.rxn_smi)
+    else:
+        raise ValueError(f"No smiles column in input file. headers: {df_solute.columns}")
+
     mol_id_to_mol_smi = dict(zip(mol_ids, mol_smis))
 
     df_solvent = pd.read_csv(solvent_path)
     solvent_name_to_smi = dict(zip(df_solvent.cosmo_name, df_solvent.smiles))
 
     tar_file_paths = []
-    path = "/home/gridsan/oscarwu/co2_capture_shared/cosmo_backup/cosmo_ts_sep1a_backup"
-    for root, dirs, files in os.walk(path):
+    for root, dirs, files in os.walk(output_dir):
         for file in files:
-            print(file)
             if file.endswith(".tar"):
                 tar_file_paths.append(os.path.join(root, file))
 
@@ -65,10 +68,10 @@ def main(input_smiles_path, output_file_name, n_jobs, solvent_path):
     with open(os.path.join(submit_dir, f"{output_file_name}.pkl"), "wb") as outfile:
         pkl.dump(df_cosmo, outfile, protocol=pkl.HIGHEST_PROTOCOL)
 
-    with open(
-        os.path.join(submit_dir, f"{output_file_name}_failed.pkl"), "wb"
-    ) as outfile:
-        pkl.dump(failed_mol_ids, outfile, protocol=pkl.HIGHEST_PROTOCOL)
+    df_failed = df_solute[df_solute.id.isin(failed_mol_ids)]
+    df_failed.to_csv(
+        os.path.join(submit_dir, f"{output_file_name}_failed.csv"), index=False
+    )
 
     print(f"Total number of jobs: {len(mol_ids)}")
     print(f"Failed mol ids: {len(failed_mol_ids)}")
@@ -80,8 +83,6 @@ if __name__ == "__main__":
     output_file_name = sys.argv[2]
     n_jobs = int(sys.argv[3])
     solvent_path = sys.argv[4]
+    cosmo_output_dir = sys.argv[5]
 
-    # input_smiles_path = "reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv"
-    # n_jobs = 8
-    # output_file_name = "test"
-    main(input_smiles_path, output_file_name, n_jobs, solvent_path)
+    main(input_smiles_path, output_file_name, n_jobs, solvent_path, cosmo_output_dir)
